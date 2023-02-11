24f10ec68437904675cd7d43df6c06d7736e85bb
Denys Kozyr <dkozyr@gmail.com>
2023-02-12 00:06:23 +0200

    test float eq

--- test/fixtures/vec3Test.h
+++ test/fixtures/vec3Test.h
@@ -3,21 +3,3 @@
 
 using namespace lalgebra;
 
-class vec3Test : public ::testing::Test {
-protected:
-    vec3 v0_;
-    vec3 v1_;
-    vec3 v2_;
-    vec3 v3_;
-    vec3 v4_;
-
-protected:
-    void SetUp() override {
-        v2_.x = 2;
-        v3_.x = 2;
-        v3_.y = 3;
-        v4_.x = 2;
-        v4_.y = 4;
-        v4_.z = 4;
-    }
-};
--- test/vec3test.cpp
+++ test/vec3test.cpp
@@ -1,5 +1,40 @@
 #include "fixtures/vec3Test.h"
 
+class vec3Test : public ::testing::Test {
+protected:
+    static constexpr auto kEps = 1e-6;
+
+protected:
+    vec3 v0_;
+    vec3 v1_;
+    vec3 v2_;
+    vec3 v3_;
+    vec3 v4_;
+
+protected:
+    void SetUp() override {
+        v2_.x = 2;
+        v3_.x = 2;
+        v3_.y = 3;
+        v4_.x = 2;
+        v4_.y = 4;
+        v4_.z = 4;
+    }
+
+    void AssertEqual(const vec3& a, const vec3& b) {
+        ASSERT_NEAR(a.x, b.x, kEps);
+        ASSERT_NEAR(a.y, b.y, kEps);
+        ASSERT_NEAR(a.z, b.z, kEps);
+    }
+};
+
+TEST_F(vec3Test, equal) {
+    auto a = vec3(1, 2, 3);
+    auto b = vec3(1, 2, 3);
+    ASSERT_NO_FATAL_FAILURE(AssertEqual(a, b));
+    ASSERT_NO_FATAL_FAILURE(AssertEqual(a, vec3(3, 2, 1))); // fail
+}
+
 TEST_F(vec3Test, operatorEqualsTest) {
     EXPECT_TRUE(v0_ == v0_);
     EXPECT_TRUE(v0_ == v1_);
